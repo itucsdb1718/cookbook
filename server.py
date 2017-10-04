c09d06704d2f29fb07802c5c0666688b3cf03070
hasgule <eliifhsgl@gmail.com>
2017-10-04 21:01:10 +0300

    Bootstrap Settings

--- server.py
+++ server.py
@@ -13,6 +13,18 @@ def home_page():
     now = datetime.datetime.now()
     return render_template('home.html', current_time=now.ctime())
 
+@app.route('/profile')
+def profile_page():
+    return render_template('profile.html')
+
+@app.route('/recipes')
+def recipes_page():
+    return render_template('recipes.html')
+
+@app.route('/contact')
+def contact_page():
+    return render_template('contact.html')
+
 
 if __name__ == '__main__':
     VCAP_APP_PORT = os.getenv('VCAP_APP_PORT')
