a99f7c669f46d37e5d1b985a788ccc949f9fa597
KadirEmreOto <emrekadiroto@hotmail.com>
2017-12-09 01:34:05 +0300

    implemented relation model and edited profile page

--- cookbook/urls.py
+++ cookbook/urls.py
@@ -5,11 +5,11 @@ from . import views
 
 
 cookbook.add_url_rule('/', view_func=views.home_page)
-cookbook.add_url_rule('/profile/', view_func=views.profile_page)
+cookbook.add_url_rule('/<username>', view_func=views.profile_page)
 cookbook.add_url_rule('/recipes/', view_func=views.recipes_page)
 cookbook.add_url_rule('/contact/', view_func=views.contact_page)
 cookbook.add_url_rule('/initdb/', view_func=views.initdb)
-cookbook.add_url_rule('/uploads/<filename>', view_func=views.uploaded_file, methods=['GET'], endpoint='uploads')
+cookbook.add_url_rule('/uploads/<filename>/', view_func=views.uploaded_file, methods=['GET'], endpoint='uploads')
 cookbook.add_url_rule('/upload/', view_func=views.upload_profile_image, methods=['GET', 'POST'])
 cookbook.add_url_rule('/login/', view_func=views.login, methods=['POST'])
 cookbook.add_url_rule('/logout/', view_func=views.logout)
--- cookbook/views.py
+++ cookbook/views.py
@@ -2,18 +2,18 @@ import os
 import datetime
 
 from flask.helpers import url_for
-from flask import render_template, redirect, flash
+from flask import render_template, redirect, flash, abort
 from flask import request, current_app, send_from_directory
 from flask_login.utils import login_required, login_user, current_user, logout_user
 
-from .models import Ingredient, Recipe, Users, Message
+from .models import Ingredient, Recipe, Users, Message, Post, Relation
 
 
 def initdb():
-    for model in [Users, Recipe, Ingredient, Message]:
+    for model in [Users, Recipe, Ingredient, Message, Post, Relation]:
         model.drop()
 
-    for model in [Users, Recipe, Ingredient, Message]:
+    for model in [Users, Recipe, Ingredient, Message, Post, Relation]:
         model.create()
 
     emre = Users(username='KEO', firstname='Kadir Emre', lastname='Oto', email='otok@itu.edu.tr')
@@ -25,10 +25,12 @@ def initdb():
     emre.save()
     suheyl.save()
 
+    emre.follow(suheyl)
+
     print(emre.check_password("keo123"))
     print(emre.check_password("emre123"))
 
-    emre.username = '<KEO>'
+    emre.username = 'KadirEmreOto'
     emre.save()
 
     message = Message(_from=emre, _to=suheyl, content='Test Message 123')
@@ -60,7 +62,6 @@ def initdb():
     pirinc.save()
     su.save()
 
-
     return redirect(url_for('cookbook.home_page'))
 
 
@@ -69,9 +70,17 @@ def home_page():
     return render_template('layout.html', current_time=now.ctime())
 
 
-@login_required
-def profile_page():
-    recipes = Recipe.get(limit=None, _user=current_user, prefetch=Ingredient.recipe)
+def profile_page(username):
+    user = Users.get(limit=1, username=username)
+    if not user:
+        abort(404)
+
+    user = user[0]
+    recipes = Recipe.get(limit=None, _user=user, prefetch=Ingredient.recipe)
+
+    followers = user.get_followers()
+    following = user.get_followings()
+
     return render_template('profile.html', **locals())
 
 
@@ -121,7 +130,7 @@ def upload_profile_image():
             current_user.picture = filename
             current_user.save()
 
-            return redirect(url_for('cookbook.profile_page'))
+            return redirect(url_for('cookbook.profile_page', username=current_user.username))
 
     else:
         return '''
